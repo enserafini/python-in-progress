b1b3ce287475f5dadd373ebacaad0b30e74d418d
Enzo <enzo.serafini.01@gmail.com>
2022-01-02 13:17:31 -0300

    adding diff float

--- src/myDiff.py
+++ src/myDiff.py
@@ -1,8 +1,13 @@
-def myDivFl(a,b):
+def myDivInt(a,b):
     c=int(a/b)
     print(c)
-
+    
+def myDivFl(a,b):
+    c=a/b
+    print(c)
+    
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
+    myDivInt(a,b)
     myDivFl(a,b)
